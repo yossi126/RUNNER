ffea913474098df2e07da239d476d946eec5a9b3
shukimi <shuki6589@gmail.com>
2022-04-12 21:26:44 +0300

    V1

--- app/src/main/java/com/example/runner/MainActivity.java
+++ app/src/main/java/com/example/runner/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.runner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signUpBtn;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpBtn=findViewById(R.id.sign_main_activity);
+        loginBtn=findViewById(R.id.login_main_activity);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //LONG  NEW INTENT
+               Intent intent = new Intent(MainActivity.this,Register.class);
+                startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // SHORT  NEW INTENT
+                startActivity(new Intent(MainActivity.this,Login.class));
+            }
+        });
     }
+
 }
