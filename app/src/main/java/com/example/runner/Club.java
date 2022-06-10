037739f415cc3b341bf8afd7856d95211d746ccb
shukimi <shuki6589@gmail.com>
2022-06-10 14:35:26 +0300

    V2-10/06/2022

--- app/src/main/java/com/example/runner/Club.java
+++ app/src/main/java/com/example/runner/Club.java
@@ -1,18 +1,17 @@
 package com.example.runner;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.example.runner.Adapter.ClubAdapterRecyclerView;
-import com.example.runner.Profile;
 import com.example.runner.data.User;
 import com.example.runner.databinding.ActivityClubBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -40,7 +39,7 @@ public class Club extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_club);
+
         binding = ActivityClubBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
@@ -50,72 +49,25 @@ public class Club extends AppCompatActivity {
         //userID = currentFirebaseUser.getUid();
         userArrayList = new ArrayList<>();
 
-        //setting up top nav bar
-        binding.topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.logOut:
-                        Intent intent = new Intent(Club.this,MainActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        databaseReference.child(currentFirebaseUser.getUid()).child("isConnected").setValue(false).addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Toast.makeText(Club.this,"user is logout",Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                        FirebaseAuth.getInstance().signOut();
-                        startActivity(intent);
-                        break;
-                }
-                return true;
-            }
-        });
+        //SETTING TOP NAV BAR
+        topNavBar();
 
-        //setting up the bottom nav
-        binding.bottomNavBar.setSelectedItemId(R.id.club); // to keep the icon on
-        binding.bottomNavBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.home_page:
-                        Intent intent = new Intent(Club.this,HomePage.class);
-                        startActivity(intent);
-                        overridePendingTransition(0, 0);
-                        //finish();
-                        break;
-                    case R.id.club:
-                        break;
-                    case R.id.run:
-                        Intent intentR = new Intent(Club.this,Run.class);
-                        startActivity(intentR);
-                        overridePendingTransition(0, 0);
-                        break;
-                    case R.id.status:
-                        break;
-                    case R.id.profile:
-                        Intent intentP = new Intent(Club.this, Profile.class);
-                        startActivity(intentP);
-                        overridePendingTransition(0, 0);
-                        break;
-                }
-                return true;
-            }
-        });
+        //SETTING BOTTOM NAV BAR
+        bottomNavBar();
 
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                    userArrayList.clear();
-                    for (DataSnapshot ds : snapshot.getChildren()){
-                        User user = ds.getValue(User.class);
-                        //userArrayList.add(user);
-                        if(!currentFirebaseUser.getUid().equals(user.getUid())){
-                            userArrayList.add(user);
-                        }
+                userArrayList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    User user = ds.getValue(User.class);
+                    //userArrayList.add(user);
+                    if (!currentFirebaseUser.getUid().equals(user.getUid())) {
+                        userArrayList.add(user);
                     }
+                }
 
                 clubAdapterRecyclerView = new ClubAdapterRecyclerView(userArrayList);
                 binding.clubRV.setHasFixedSize(true);
@@ -130,9 +82,63 @@ public class Club extends AppCompatActivity {
         });
 
 
+    }
 
+    private void bottomNavBar() {
+        //setting up the bottom nav
+        binding.bottomNavBar.setSelectedItemId(R.id.club); // to keep the icon on
+        binding.bottomNavBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_page:
+                        Intent intent = new Intent(Club.this, HomePage.class);
+                        startActivity(intent);
+                        overridePendingTransition(0, 0);
+                        //finish();
+                        break;
+                    case R.id.club:
+                        break;
+                    case R.id.run:
+                        Intent intentR = new Intent(Club.this, Run.class);
+                        startActivity(intentR);
+                        overridePendingTransition(0, 0);
+                        break;
+                    case R.id.status:
+                        break;
+                    case R.id.profile:
+                        Intent intentP = new Intent(Club.this, Profile.class);
+                        startActivity(intentP);
+                        overridePendingTransition(0, 0);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
 
-
+    //setting up top nav bar
+    private void topNavBar() {
+        binding.topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.logOut:
+                        Intent intent = new Intent(Club.this, MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        databaseReference.child(currentFirebaseUser.getUid()).child("isConnected").setValue(false).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(Club.this, "user is logout", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 
