9d94a273066c890a84549a468c054f4139ef0747
shukimi <shuki6589@gmail.com>
2022-06-15 17:01:50 +0300

    V2-15/06/2022

--- app/src/main/java/com/example/runner/EndRunSummary.java
+++ app/src/main/java/com/example/runner/EndRunSummary.java
@@ -1,17 +1,16 @@
 package com.example.runner;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.runner.databinding.ActivityEndRunSummaryBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,23 +20,18 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -71,8 +65,6 @@ public class EndRunSummary extends AppCompatActivity {
     private DocumentReference documentReference;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,7 +83,7 @@ public class EndRunSummary extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
         firebaseFirestore = FirebaseFirestore.getInstance();
-        documentReference =  firebaseFirestore.collection(firebaseUser.getUid()).document(getCurrentDateTime());
+        documentReference = firebaseFirestore.collection(firebaseUser.getUid()).document(getCurrentDateTime());
 
 
         if (extras != null) {
@@ -122,13 +114,13 @@ public class EndRunSummary extends AppCompatActivity {
         documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
             public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                if(error != null){
+                if (error != null) {
                     Toast.makeText(EndRunSummary.this, "Error!", Toast.LENGTH_SHORT).show();
                     Log.d("EndRunSummary", error.toString());
                     return;
                 }
 
-                if(value.exists()){
+                if (value.exists()) {
                     //Log.d("EndRunSummary1", value.getString("date"));
                     //Log.d("EndRunSummary2", value.toString());
                 }
@@ -198,7 +190,7 @@ public class EndRunSummary extends AppCompatActivity {
         return datetime;
     }
 
-        private static String getCurrentDate() {
+    private static String getCurrentDate() {
         // creating an id by time and date to the "SavedRuns" children
         Date dNow = new Date();
         SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
