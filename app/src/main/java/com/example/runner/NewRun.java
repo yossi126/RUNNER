454236c87b58df489e200945e985d06d9275b614
shukimi <shuki6589@gmail.com>
2022-07-16 23:06:11 +0300

    V2-16/07/2022

--- app/src/main/java/com/example/runner/NewRun.java
+++ app/src/main/java/com/example/runner/NewRun.java
@@ -1,6 +1,8 @@
 package com.example.runner;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Build;
@@ -8,12 +10,14 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.runner.data.Splits;
 import com.example.runner.databinding.ActivityNewRunBinding;
@@ -31,6 +35,8 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -105,6 +111,7 @@ public class NewRun extends AppCompatActivity implements View.OnClickListener {
         timerStarted = false;
 
 
+
 //        focus.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -153,12 +160,14 @@ public class NewRun extends AppCompatActivity implements View.OnClickListener {
         //setting up stop button
         stopButton();
 
+        topNavBar();
+
     }
 
-    /* 
+    /*
              ----------------------------------   timer for splits methods -----------------------------------------------------
-     
-     
+
+
      */
 
     public void ResetSplitTimer() {
@@ -214,14 +223,7 @@ public class NewRun extends AppCompatActivity implements View.OnClickListener {
         return String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
     }
 
-
-    /* 
-                ------------------------------------ end - timer for splits methods -------------------------------------------------------
-     
-     
-     */
-
-    // method for calculate the splits of each km 
+    // method for calculate the splits of each km
     private void calcSplits(double track) {
         if (track > kmToran) {
             currentSplit = getSplitTimerText();
@@ -237,6 +239,12 @@ public class NewRun extends AppCompatActivity implements View.OnClickListener {
         }
     }
 
+    /*
+                ------------------------------------ end - timer for splits methods -------------------------------------------------------
+
+
+     */
+
 
     private void stopButton() {
         binding.imageButtonStop.setOnLongClickListener(new View.OnLongClickListener() {
@@ -250,7 +258,7 @@ public class NewRun extends AppCompatActivity implements View.OnClickListener {
                 intent.putExtra("splits", splitsArrayList);
                 startActivity(intent);
                 finish();
-                // ending the split timer 
+                // ending the split timer
                 timerTask.cancel();
                 return false;
             }
@@ -264,6 +272,133 @@ public class NewRun extends AppCompatActivity implements View.OnClickListener {
         locationRequest.setInterval(1000);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, looper.myLooper());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.imageButtonStartPause) {
+            startPauseCycle();
+        } else if (v.getId() == R.id.imageButtonStop) {
+            Toast.makeText(this, "Ending run with long click", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //SETTING TOP NAV BAR - CANCEL RUN BUTTON
+    private void topNavBar() {
+        //SETTING TOP NAV BAR
+        binding.topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.cancelNewRun:
+                        startPauseCycle();
+                        MaterialAlertDialogBuilder mainBuilder = new MaterialAlertDialogBuilder(NewRun.this);
+                        mainBuilder.setTitle("")
+                                .setMessage("Do you wish to cancel run (activity will not be saved)?")
+                                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(NewRun.this, Run.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        startPauseCycle();
+                                    }
+                                }).create();
+                        mainBuilder.show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    //PAUSE RUN - CONTINUE RUN
+    private void startPauseCycle() {
+        if (!stopStart) {
+            stopStart = true;
+            startChronometer();
+            requestLocation();
+            StartStopSplitTimer();
+            startPauseButton.setImageResource(R.drawable.ic_baseline_pause_24);
+        } else {
+            stopStart = false;
+            pauseChronometer();
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+            startPauseButton.setImageResource(R.drawable.ic_baseline_play_arrow_24);
+            StartStopSplitTimer();
+        }
+    }
+
+    // cancel the split timer on back press
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        timerTask.cancel();
+    }
+
+    /*
+         ----------------------------------   SETTING RUN PARAMETERS -----------------------------------------------------
+
+
+ */
+
+    // CHRONOMETER
+    private void startChronometer() {
+        binding.timeChronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+        binding.timeChronometer.start();
+    }
+
+    private void pauseChronometer() {
+        binding.timeChronometer.stop();
+        pauseOffset = SystemClock.elapsedRealtime() - binding.timeChronometer.getBase();
+    }
+
+    private void resetChronometer() {
+        binding.timeChronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+
+    private void getCurrentLocation() {
+        // init the first location to avoid mistakes
+        @SuppressLint("MissingPermission") Task<Location> task = fusedLocationProviderClient.getLastLocation();
+    }
+
+    //DISTANCE IN KM
+    public float distance(float lat_a, float lng_a, float lat_b, float lng_b) {
+        // calculate the distance between 2 points
+        double earthRadius = 3958.75;
+        double latDiff = Math.toRadians(lat_b - lat_a);
+        double lngDiff = Math.toRadians(lng_b - lng_a);
+        double a = Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
+                Math.cos(Math.toRadians(lat_a)) * Math.cos(Math.toRadians(lat_b)) *
+                        Math.sin(lngDiff / 2) * Math.sin(lngDiff / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = earthRadius * c;
+        int meterConversion = 1609;
+        return new Float(distance * meterConversion).floatValue();
+    }
+
+    //POLYLINE TRACK
+    public void getFarAway(PolylineOptions road) {
+        // calculating user's run by list of points
+        List<LatLng> locations = road.getPoints();
+        Object[] points = locations.toArray();
+        length = points.length - 1;
+        if (length > 0 && points[length - 1] instanceof LatLng && points[length] instanceof LatLng) {
+            LatLng latlng1, latLng2;
+            latlng1 = (LatLng) points[length - 1];
+            latLng2 = (LatLng) points[length];
+            lat1 = (float) latlng1.latitude;
+            lng1 = (float) latlng1.longitude;
+            lat2 = (float) latLng2.latitude;
+            lng2 = (float) latLng2.longitude;
+            distanceFar += distance(lat1, lng1, lat2, lng2);
+        }
     }
 
     private void drawTrack() {
@@ -292,86 +427,4 @@ public class NewRun extends AppCompatActivity implements View.OnClickListener {
         });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.imageButtonStartPause) {
-            if (!stopStart) {
-                stopStart = true;
-                startChronometer();
-                requestLocation();
-                startPauseButton.setImageResource(R.drawable.ic_baseline_pause_24);
-                StartStopSplitTimer();
-            } else {
-                stopStart = false;
-                pauseChronometer();
-                fusedLocationProviderClient.removeLocationUpdates(locationCallback);
-                startPauseButton.setImageResource(R.drawable.ic_baseline_play_arrow_24);
-                StartStopSplitTimer();
-
-            }
-        } else if (v.getId() == R.id.imageButtonStop) {
-            Toast.makeText(this, "Ending run with long click", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    // chronometer
-    private void startChronometer() {
-        binding.timeChronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
-        binding.timeChronometer.start();
-    }
-
-    private void pauseChronometer() {
-        binding.timeChronometer.stop();
-        pauseOffset = SystemClock.elapsedRealtime() - binding.timeChronometer.getBase();
-    }
-
-    private void resetChronometer() {
-        binding.timeChronometer.setBase(SystemClock.elapsedRealtime());
-        pauseOffset = 0;
-    }
-
-    private void getCurrentLocation() {
-        // init the first location to avoid mistakes
-        @SuppressLint("MissingPermission") Task<Location> task = fusedLocationProviderClient.getLastLocation();
-    }
-
-    public float distance(float lat_a, float lng_a, float lat_b, float lng_b) {
-        // calculate the distance between 2 points
-        double earthRadius = 3958.75;
-        double latDiff = Math.toRadians(lat_b - lat_a);
-        double lngDiff = Math.toRadians(lng_b - lng_a);
-        double a = Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
-                Math.cos(Math.toRadians(lat_a)) * Math.cos(Math.toRadians(lat_b)) *
-                        Math.sin(lngDiff / 2) * Math.sin(lngDiff / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = earthRadius * c;
-        int meterConversion = 1609;
-        return new Float(distance * meterConversion).floatValue();
-    }
-
-    public void getFarAway(PolylineOptions road) {
-        // calculating user's run by list of points
-        List<LatLng> locations = road.getPoints();
-        Object[] points = locations.toArray();
-        length = points.length - 1;
-        if (length > 0 && points[length - 1] instanceof LatLng && points[length] instanceof LatLng) {
-            LatLng latlng1, latLng2;
-            latlng1 = (LatLng) points[length - 1];
-            latLng2 = (LatLng) points[length];
-            lat1 = (float) latlng1.latitude;
-            lng1 = (float) latlng1.longitude;
-            lat2 = (float) latLng2.latitude;
-            lng2 = (float) latLng2.longitude;
-            distanceFar += distance(lat1, lng1, lat2, lng2);
-        }
-    }
-
-    // cancel the split timer on back press
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        timerTask.cancel();
-    }
 }
